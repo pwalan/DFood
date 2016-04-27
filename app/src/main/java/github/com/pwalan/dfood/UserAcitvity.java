09e223e7065bd4407e41f437a5c78b30179f0ea5
AlanP <pwalan@qq.com>
2016-04-27 18:13:24 +0800

    some change

--- app/src/main/java/github/com/pwalan/dfood/UserAcitvity.java
+++ app/src/main/java/github/com/pwalan/dfood/UserAcitvity.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 /**
  * 用户登录/注册
  */
-public class UserAcitvity extends Activity {
+public class UserAcitvity extends Activity implements View.OnClickListener{
 
     private String passwd;
     private String passwdconf;
@@ -30,6 +31,35 @@ public class UserAcitvity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        et_username=(EditText)findViewById(R.id.et_username);
+        et_passwd=(EditText)findViewById(R.id.et_passwd);
+        et_passwdconf=(EditText)findViewById(R.id.et_passwdconf);
+
+        btn_login=(Button)findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+        btn_register=(Button)findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(this);
+        btn_toregister=(Button)findViewById(R.id.btn_toregister);
+        btn_toregister.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login:
+                break;
+            case R.id.btn_toregister:
+                //点击去注册后此按钮和登录按钮隐藏，确认密码和注册显示
+                btn_toregister.setVisibility(View.INVISIBLE);
+                btn_login.setVisibility(View.INVISIBLE);
+                et_passwdconf.setVisibility(View.VISIBLE);
+                btn_register.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn_register:
+                break;
+            default:
+                break;
+        }
+    }
 }
